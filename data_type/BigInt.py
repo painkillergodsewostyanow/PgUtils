4ace0c5dd1f03972e92326aaa251bfe67f2767a1
painkillergod <thebladethatbringsdeath@gmail.com>
2023-04-29 02:55:43 +0300

    add independentce and some fix

--- data_type/BigInt.py
+++ data_type/BigInt.py
@@ -1,9 +1,8 @@
 class BigInt:
-
-    LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H",
-               "I", "J", "K", "L", "M", "N", "O", "P",
-               "Q", "R", "S", "T", "U", "V", "W", "X",
-               "Y", "Z"]
+    __CHARACTERS = ["A", "B", "C", "D", "E", "F", "G", "H",
+                    "I", "J", "K", "L", "M", "N", "O", "P",
+                    "Q", "R", "S", "T", "U", "V", "W", "X",
+                    "Y", "Z"]
 
     def __init__(self, arg1=None, arg2=None, max_digit_before_letters=100):
 
@@ -14,13 +13,13 @@ class BigInt:
             self.p = arg1.p
 
         elif isinstance(arg1, int) and not arg2:
-            self.mantissa, self.p = self.get_mantissa_and_p_from_int(arg1)
+            self.mantissa, self.p = self.__get_mantissa_and_p_from_int(arg1)
 
-        elif isinstance(arg1, (int, float)) and isinstance(arg2, (int, float)):
+        elif isinstance(arg1, int) and isinstance(arg2, int):
             self.mantissa = arg1
             self.p = arg2
 
-    def get_mantissa_and_p_from_int(self, integer):
+    def __get_mantissa_and_p_from_int(self, integer):
         p = 0
         while integer > self.max_digit_before_letters:
             integer //= 10
@@ -33,20 +32,20 @@ class BigInt:
         return round(self.mantissa * 10 ** self.p)
 
     def __str__(self):
-        if self.p > 26:
+        if self.p > len(self.__CHARACTERS):
             p_copy = self.p
             count = 0
-            while p_copy > 26:
+            while p_copy > len(self.__CHARACTERS):
+                p_copy -= len(self.__CHARACTERS)
                 count += 1
-                p_copy -= 26
 
-            return f"{self.mantissa}{count * 'Z'}{self.LETTERS[p_copy-1]}"
-        return f"{self.mantissa} {self.LETTERS[self.p-1]}"
+            return f"{self.mantissa}{count * self.__CHARACTERS[-1]}{self.__CHARACTERS[p_copy - 1]}"
+        return f"{self.mantissa} {self.__CHARACTERS[self.p - 1]}"
 
 
-b1 = BigInt(25 * 10 ** 11)
+b1 = BigInt(25 * 10 ** 111)
 b2 = BigInt(b1)
-b3 = BigInt(25, 11)
+b3 = BigInt(25, 111)
 
 print(b1, b2, b3)
 print(int(b1), int(b2), int(b3), sep="\n")
